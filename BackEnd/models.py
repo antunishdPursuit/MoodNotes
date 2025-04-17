663ce82cc7350ad0f8ca1735bba46ce164f64126
Daniel Oshima <doshima2013@gmail.com>
2025-04-17 19:44:00 -0400

    add validation for user_entry field of MoodEntry model using @validates

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -6,10 +6,18 @@ class MoodEntry(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_entry = db.Column(db.String, nullable=False)
-    affirmation = db.column(db.String, nullable=False)
+    affirmation = db.Column(db.String, nullable=False)
 
     def __repr__(self):
-        return f"User Entry: {self.user_entry} returns '{self.affirmation}'"
+        return f"User Entry: '{self.user_entry}' returns '{self.affirmation}'"
     
     def to_dict(self):
         return{"id": self.id, "user_entry": self.user_entry, "affirmation": self.affirmation}
+    
+    @validates("user_entry")
+    def validate_entry(self, key, value):
+        if not isinstance(value, str):
+            raise TypeError("Entry must be a string.")
+        if not len(value.strip()) > 0:
+            raise ValueError("Entry must exist.")
+        return value
