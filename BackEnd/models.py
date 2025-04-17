fea202e4e522be61a92ad8a5c75ab352232f4f2d
Daniel Oshima <doshima2013@gmail.com>
2025-04-17 19:50:49 -0400

    add validation for affirmation field of MoodEntry model using @validates

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -21,3 +21,13 @@ class MoodEntry(db.Model):
         if not len(value.strip()) > 0:
             raise ValueError("Entry must exist.")
         return value
+    
+    @validates("affirmation")
+    def validate_affirmation(self, key, value):
+        if not isinstance(value, str):
+            raise TypeError("Affirmation must be a string.")
+        cleaned_value = value.strip()
+        if not len(cleaned_value) > 0:
+            raise ValueError("Affirmation must not be empty.")
+        return cleaned_value
+
